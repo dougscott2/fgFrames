b0c6a84ca0306f62bbac2f19612dc12184839584
DrScott <dougscott2@gmail.com>
2016-02-01 13:17:42 -0500

    Initial import

---
+++ src/main/java/com/frameData/entities/fighter.java
@@ -0,0 +1,12 @@
+package com.frameData.entities;
+
+import java.util.HashMap;
+
+/**
+ * Created by DrScott on 2/1/16.
+ */
+public class fighter {
+    public String characterName;
+    public HashMap<String, int[]> frameData;
+
+}
---
+++ src/main/java/com/frameData/utils/FgFramesApplication.java
@@ -0,0 +1,12 @@
+package com.frameData.utils;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FgFramesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FgFramesApplication.class, args);
+	}
+}
