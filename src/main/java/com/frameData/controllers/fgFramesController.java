149219bc2ae5b9db86dc7d45ae68148b1da7f827
DrScott <dougscott2@gmail.com>
2016-02-02 14:46:34 -0500

    cleared all info to start from the begining

--- src/main/java/com/frameData/controllers/fgFramesController.java
+++ src/main/java/com/frameData/controllers/fgFramesController.java
@@ -1,9 +1,5 @@
 package com.frameData.controllers;
 
-import com.frameData.entities.FrameData;
-import com.frameData.entities.fighter;
-import com.frameData.services.FrameDataRepository;
-import com.frameData.services.fighterRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
@@ -14,44 +10,6 @@ import javax.annotation.PostConstruct;
  */
 @Controller
 public class fgFramesController {
-    @Autowired
-    fighterRepository fighters;
-
-    @Autowired
-    FrameDataRepository frameDatas;
-
-
-    @PostConstruct
-    public void init(){
-        //populate database if empty
-        if (fighters.findOneByCharacterName("Cammy") ==null){
-            fighter cammy = new fighter();
-            cammy.characterName="Cammy";
-            fighters.save(cammy);
-            FrameData fm = new FrameData();
-            fm.moveName = "Standing LP";
-            fm.startUp = 3;
-            fm.active = 2;
-            fm.recovery = 4;
-            fm.blockAdvantage =5;
-            fm.damage = 10;
-            fm.hitAdvantage = 10;
-            fm.hitStop = 10;
-            fm.stun = 100;
-            fm.total = fm.startUp + fm.recovery;
-            fm.character = cammy;
-            cammy.moves.add(fm);
-            frameDatas.save(fm);
-
-
-
-        }
-
-    }
-
-    //pull current selected fighter
-
-    //pull secondary fighter data
 
 
 }
