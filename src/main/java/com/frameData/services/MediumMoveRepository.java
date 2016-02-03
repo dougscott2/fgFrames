c6df571942d9b06b48334601ea7135a078d56f48
DrScott <dougscott2@gmail.com>
2016-02-02 20:40:42 -0500

    MMRepo

--- src/main/java/com/frameData/services/MediumMoveRepository.java
+++ src/main/java/com/frameData/services/MediumMoveRepository.java
@@ -11,5 +11,5 @@ import java.util.List;
  */
 public interface MediumMoveRepository extends PagingAndSortingRepository<MediumMove, Integer>{
     List<MediumMove> findAllByFighter(Fighter fighter);
-    //dd
+    //ddasdf
 }
