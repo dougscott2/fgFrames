308f768d32150e4f5c9a94162c5dfa44c81e0b85
DrScott <dougscott2@gmail.com>
2016-02-02 20:38:14 -0500

    test commit to just comment here

--- src/main/java/com/frameData/services/MediumMoveRepository.java
+++ src/main/java/com/frameData/services/MediumMoveRepository.java
@@ -11,4 +11,5 @@ import java.util.List;
  */
 public interface MediumMoveRepository extends PagingAndSortingRepository<MediumMove, Integer>{
     List<MediumMove> findAllByFighter(Fighter fighter);
+    //dd
 }
